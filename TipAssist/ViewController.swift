586c6f523bddacc32b7dcc3bf852e57033d5586f
Praveen Murugesan <praveen@uber.com>
2017-09-26 21:20:58 -0700

    Initial tipper

--- TipAssist/ViewController.swift
+++ TipAssist/ViewController.swift
@@ -1,25 +1,39 @@
+
 //
 //  ViewController.swift
 //  TipAssist
 //
-//  Created by Praveen Murugesan on 9/26/17.
+//  Created by Praveen Murugesan on 9/25/17.
 //  Copyright © 2017 Praveen Murugesan. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+
+    @IBOutlet weak var tipLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.billAmountTextField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
 
 
+    @IBAction func calculateTip(_ sender: UITextField) {
+        if let textVal = sender.text {
+            let billVal = Double(textVal)
+            let tipVal = billVal != nil ? String(billVal! * 0.15) : nil
+            tipLabel.text =  tipVal ?? "Invalid"
+        }
+    }
+    
 }
 
